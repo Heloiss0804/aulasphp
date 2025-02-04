a67bd59daf840ed0dc79e0d046675d794bbb956c
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-04 11:16:27 -0300

    adiciona exemplos de declaração e estrutura

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -11,9 +11,32 @@
 <?php
 //Criando uma intância de uma classe genérica chamada stdClass (Standar Class)
 $usuario = new stdClass(); //instância ou objeto
+
+//Adicionando propriedades/atributos ao objeto
+$usuario-> nome = "Chapolin Colorado";
+$usuario-> idade = 25;
+$usuario-> email = "chapolim@gmail.com";
+$usuario-> telefones = ["11-2658-6666", "11-9875-5698"];
+
+echo $usuario-> nome;
+
+//Pode ser usado {}ao redor do objeto/propriedade na interpolação
+echo "<p><mark>{$usuario-> nome}</mark><p>" ; //interpolação
+
 ?>
 
 <h2>Analisando a estrutura do objeto</h2>
-<pre><?var_dump($usuario)?></pre>
+<pre><?=var_dump($usuario)?></pre>
+
+<h2>Saída de Dados</h2>
+<p>Nome: <?=$usuario->nome?></p>
+<p>Idade: <?=$usuario->idade?> anos</p>
+<p>Celular: <?=$usuario->telefones[1]?></p>
+
+<hr>
+
+
+
+
 </body>
 </html>
