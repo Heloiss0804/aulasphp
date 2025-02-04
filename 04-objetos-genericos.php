26f2c334e8fb330b61f3ae581e8be3c7fde41058
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-04 11:30:56 -0300

    converte array em objeto

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -43,6 +43,16 @@ $arrayUsuario = (array) $usuario;
 <h3>Analisando o array gerado a partir de um objeto</h3>
 <pre><?=var_dump($arrayUsuario)?></pre>
 
+<h2>Convertendo (fazendo um casting) array assossiativo em objeto</h2>
+
+<?php
+$aluno = ["nome" => "Fulano", "sobrenome" => "dos Santos"];
+
+$objAluno = (object) $aluno;
+?>
+
+<h3>Analisando o objeto gerado a partir de um array</h3>
+<pre><?=var_dump($aluno)?></pre>
 
 </body>
 </html>
