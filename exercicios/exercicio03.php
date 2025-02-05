4816fe9b705a46db6ebc72117b7a2964c141b30f
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-05 11:47:27 -0300

    exercicio corrigido

--- exercicios/exercicio03.php
+++ exercicios/exercicio03.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 03</title>
+    <title>Exercício 03</title>
 </head>
 
 <body>
@@ -21,6 +21,8 @@
         $desconto = 0.10;
     } elseif ($valor_compra > 1000) {
         $desconto = 0.05;
+    } else{
+        $desconto = 0;
     }
 
     $valor_com_desconto = $valor_compra - ($valor_compra * $desconto);
@@ -29,7 +31,8 @@
     
     ?>
 
-    <p>O valor da compra feita é <?=$valor_compra?> e o desconto obtido foi <?=$desconto?></p>
+    <p>O valor da compra feita é <?=$valor_compra?> e o desconto obtido foi <?=$valor_final_formatado?></p>
+
 
 </body>
 
