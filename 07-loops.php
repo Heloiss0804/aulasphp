b0843981b92d87afbea6c7b302d5c27fb0c15f6c
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-06 11:10:40 -0300

    add exemplo foreach

--- 07-loops.php
+++ 07-loops.php
@@ -1,57 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Loops</title>
 </head>
+
 <body>
     <h1>Trabalhando com comandos de repetição</h1>
     <hr>
 
     <h2>WHILE (Enquanto)</h2>
     <p>Executa ações <b>enquanto a condição for verdadeira</b>. Se a condição inicialmente não é verdadeira, o loop não funciona nenhuma vez.</p>
- <?php
-$i = 1;
-while ($i <= 3){   
- ?> 
- <p> <?=$i?></p>
- <?php
- $i++;
-}
- ?>   
+    <?php
+    $i = 1;
+    while ($i <= 3) {
+    ?>
+        <p> <?= $i ?></p>
+    <?php
+        $i++;
+    }
+    ?>
 
-<hr>
+    <hr>
 
     <h2>DO/WHILE (parecido com REPITA) </h2>
     <p>Repete <b>pelo menos uma vez</b>as ações e, a caso a condição continue verdadeira, segue fazendo outros ciclos de repetição até a condição se tornar falsa.</p>
 
-<?php
-$j =  1;
-do {
+    <?php
+    $j =  1;
+    do {
 
-?> 
-   <div>
-    <h3>Título...</h3>
-    <p>Texto Qualquer...</p>
-   </div>
-<?php
-$j++;
-} while ($j <= 3);
-?>   
+    ?>
+        <div>
+            <h3>Título...</h3>
+            <p>Texto Qualquer...</p>
+        </div>
+    <?php
+        $j++;
+    } while ($j <= 3);
+    ?>
 
-<hr>
+    <hr>
 
     <h2>FOR (PARA)</h2>
     <p>Executa ações por uma <b>quantidade determinada</b>de vezes.</p>
 
-<?php
-for($i = 1; $i <= 10; $i++){
-?>
-    <p><code> i </code>vale: <b><?=$i?> </b></p>
-<?php
-}
-?>
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+    ?>
+        <p><code> i </code>vale: <b><?= $i ?> </b></p>
+    <?php
+    }
+    ?>
 
     <hr>
     <h2>Mini-exercício</h2>
@@ -60,25 +62,52 @@ for($i = 1; $i <= 10; $i++){
 
     <ol>
         <?php
-           
-            $meses = [
-                "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
-                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
-            ];
 
-            
-            $itensLista = '';
+        $meses = [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"
+        ];
 
-           
-            for ($i = 0; $i < count($meses); $i++) {
-                $itensLista .= "<li>" . $meses[$i] . "</li>";
-            }
 
-            echo $itensLista;
+        $itensLista = '';
+
+
+        for ($i = 0; $i < count($meses); $i++) {
+            $itensLista .= "<li>" . $meses[$i] . "</li>";
+        }
+
+        echo $itensLista;
         ?>
     </ol>
 
+    <hr>
+    <h2>FOREACH (PARA CADA)</h2>
+    <p>Versão simplificada do loop for e bem mais fácil de usar com arrays e objetos.</p>
 
-    
+    <h3>Acessando array indexado/numérico</h3>
+    <?php
+    $alunos = ["Denis", "Paulo", "Heloisa"];
+
+    //     (nomeDoArray as nome DeVariavelDeacesso)
+    foreach ($alunos as $aluno) {
+    ?>
+        <p>Nome: <?= $aluno ?></p>
+    <?php
+    }
+    ?>
+
+
+
 </body>
+
 </html>
