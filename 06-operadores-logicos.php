9beb9e3dd4093533284169f635efb7da8357a2c8
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-06 08:39:24 -0300

    exemplo OU/OR

--- 06-operadores-logicos.php
+++ 06-operadores-logicos.php
@@ -22,9 +22,23 @@ if ($media >= 7 && $faltas <= 10){
     echo "<p>Reprovado!</p>";
 }
 ?>
+ <hr>
 
     <!-- o símbolo | é chamado de pipe -->
     <h2>|| (OU/OR)</h2>
+    <p><i>Basta que apenas uma condição seja <b>VERDADEIRA/TRUE</b></i></p>
+<?php
+//Dar um desconto a um cliente que seja VIP ou qye tenha cupom
+$clientesVIP = true; //valor/tipo lógico/bolean 
+$temCupom = false;
+
+if ($clientesVIP || $temCupom){
+    echo "<p>Desconto aplicado </p>";
+}else {
+    echo "<p>Sem desconto!</p>";
+
+}
+?>    
 
     <h2>! (NÃO/NOT)</h2>
 
