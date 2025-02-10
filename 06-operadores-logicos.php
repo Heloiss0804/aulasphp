591895db8d40781116f1a42f95b49b0f456e282b
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-10 11:26:37 -0300

    adiciona recursos bootstrap

--- 06-operadores-logicos.php
+++ 06-operadores-logicos.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Operadores Lógicos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Relembrando operadores Lógicos</h1>
-    <hr>
+    <div class="container">
+    <h1 class="alert alert-success-primary">Relembrando operadores Lógicos</h1>
+    <hr >
 
-    <h2>&& (E/AND)</h2>
-    <p><i>Todas as condições precisam ser <b>VERDADEIRAS/TRUE</b></i></p>
+    <h2  class="alert alert-danger" role="alert">&& (E/AND)</h2>
+    <p class="alert alert-warning" role="alert"><i>Todas as condições precisam ser <b>VERDADEIRAS/TRUE</b></i></p>
 <?php
 // Avaliar um aluno mediante média e faltas 
 $media = 9.5;
@@ -19,7 +21,7 @@ $faltas = 10;
 if ($media >= 7 && $faltas <= 10){
     ?>
     
-    <p>Aprovado!</p>
+    <p class="alert-heading">Aprovado!</p>
     <?php
 }else {
     ?>
@@ -30,7 +32,7 @@ if ($media >= 7 && $faltas <= 10){
  <hr>
 
     <!-- o símbolo | é chamado de pipe -->
-    <h2>|| (OU/OR)</h2>
+    <h2 class="alert alert-warning" role="alert">|| (OU/OR)</h2>
     <p><i>Basta que apenas uma condição seja <b>VERDADEIRA/TRUE</b></i></p>
 <?php
 //Dar um desconto a um cliente que seja VIP ou qye tenha cupom
@@ -50,7 +52,7 @@ if ($clientesVIP || $temCupom){
 ?>    
 <hr>
 
-    <h2>! (NÃO/NOT)</h2>
+    <h2 class="alert alert-success" role="alert">! (NÃO/NOT)</h2>
     <p><i>É uma inversão de lógica: VERDADEIRA vira <b>FALSO</b>, FALSO vira <b>VERDADEIRO</b></i></p>
 <?php
 //Se o usuário NÃO ESTIVER logado, exibir o lunk/botão de LOGIN.
@@ -66,7 +68,7 @@ if (!$usuarioLogado) {
 ?>    
 
 <hr>
-<h2>Usando os 3 operadores combinados para uma lógica mais elaborada</h2>
+<h2 class="alert alert-dark" role="alert">Usando os 3 operadores combinados para uma lógica mais elaborada</h2>
 
 <?php
 //Para entrar numa festa é necessário os seguintes criterios:
@@ -94,7 +96,8 @@ if(($idade >= 18 || $acompanhadoDosPais) && !$estaBebado)
 }
 ?>
 
+</div>
 
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
 </body>
 </html>
--- exercicios/exercicio04.php
+++ exercicios/exercicio04.php
@@ -1,13 +1,14 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Exercicio - 04</title>
-<style>
-    
-       table {
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio - 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <style> -->
+
+    <!-- table { 
            width: 60%;
            margin: 50px auto;
            border-collapse: collapse;
@@ -40,89 +41,78 @@
         justify-content: center;
        
        }
-</style>
+</style>-->
 </head>
-<body>
-<table class="tabela-linguagens">
-<thead>
-<tr>
-<th>ID</th>
-<th>Linguagem</th>
-<th>Descrição</th>
-</tr>
-</thead>
-<tbody>
-  <h1>Tabela de linguagens e descricao</h1>
-<?php
 
-$linguagens = [
-  [
-      "id" => 1,
-      "nome" => "HTML",
-      "descricao" => "Estruturação"
-  ],
-  [
-      "id" => 2,
-      "nome" => "CSS",
-      "descricao" => "Estilos"
-  ],
-  [
-      "id" => 3,
-      "nome" => "JS",
-      "descricao" => "Comportamentos"
-  ],
-  [
-      "id" => 4,
-      "nome" => "PHP",
-      "descricao" => "Back-End"
-  ],
-  [
-      "id" => 5,
-      "nome" => "SQL",
-      "descricao" => "Manipulação de dados"
-  ],
-  [
-      "id" => 6,
-      "nome" => "Java",
-      "descricao" => "Softwares"
-  ],
-];
-?>
-<?php
-          
-           
-          
-           foreach ($linguagens as  $linguagem) {
+<body >
+    <table class="table table-dark table-hover">
+        <thead class="">
+            <tr class="">
+                <th class="">ID</th>
+                <th class="">Linguagem</th>
+                <th class="">Descrição</th>
+            </tr>
+        </thead>
+        <tbody>
+            <h1 class=>Tabela de linguagens e descricao</h1>
+            <?php
+
+            $linguagens = [
+                [
+                    "id" => 1,
+                    "nome" => "HTML",
+                    "descricao" => "Estruturação"
+                ],
+                [
+                    "id" => 2,
+                    "nome" => "CSS",
+                    "descricao" => "Estilos"
+                ],
+                [
+                    "id" => 3,
+                    "nome" => "JS",
+                    "descricao" => "Comportamentos"
+                ],
+                [
+                    "id" => 4,
+                    "nome" => "PHP",
+                    "descricao" => "Back-End"
+                ],
+                [
+                    "id" => 5,
+                    "nome" => "SQL",
+                    "descricao" => "Manipulação de dados"
+                ],
+                [
+                    "id" => 6,
+                    "nome" => "Java",
+                    "descricao" => "Softwares"
+                ],
+            ];
             ?>
-               <tr>
-              <td><?=$linguagem["id"]?></td>
-              <td><?=$linguagem["nome"]?></td>
-              <td><?=$linguagem["descricao"]?></td>
-              </tr>
-              
-           
-                <?php
-                 
-           }
-           ?>
-</tbody>
-</table>
+            <?php
+
+
+
+            foreach ($linguagens as  $linguagem) {
+            ?>
+                <tr>
+                    <td><?= $linguagem["id"] ?></td>
+                    <td><?= $linguagem["nome"] ?></td>
+                    <td><?= $linguagem["descricao"] ?></td>
+                </tr>
+
+
+            <?php
+
+            }
+            ?>
+
+
+        </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
-
-
-
-
-
-  
-        
-
-
-
-
-
-
-
-
-
-
