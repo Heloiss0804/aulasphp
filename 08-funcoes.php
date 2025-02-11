365f33a6c8b7d7577eeaa0263bc84350ee9f15d8
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-11 08:23:02 -0300

    modificando exemplos de chamada de dados e outras chamadas

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -25,12 +25,26 @@ function exibirDadosDoAutor (){
 ?>
 
    <h3>Chamada de procedimento</h3>
-   <?=exibirDadosDoAutor()?>
+   <?php
+   $valor = 500;
+   if($valor > 1000){
+    exibirDadosDoAutor();
+  
+   }
+   ?>
+
+
+   <h3>Outras chamadas</h3>
+   <div><?=exibirDadosDoAutor()?></div>
+   <ul>
+    <li><?=exibirDadosDoAutor()?></li>
+    <li><b><?=exibirDadosDoAutor()?></b></li>
+   </ul>
 
     </div>
 
 
-
+<hr>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
