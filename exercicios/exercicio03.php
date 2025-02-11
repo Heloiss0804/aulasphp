785a2c580290442a44477663eb8555be4f0ca7e7
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-11 08:23:56 -0300

    Update exercicio03.php

--- exercicios/exercicio03.php
+++ exercicios/exercicio03.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 03</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Exercício 03 (Condicionais)</h1>
+    <h1 >Exercício 03 (Condicionais)</h1>
 
     <?php
 
@@ -33,7 +34,7 @@
 
     <p>O valor da compra feita é <?=$valor_compra?> e o desconto obtido foi <?=$valor_final_formatado?></p>
 
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>  
 
 </body>
 
