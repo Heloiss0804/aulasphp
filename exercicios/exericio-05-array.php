c1aa3772ac4ba1c25655b696ab60ce1260c3b609
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-12 11:20:56 -0300

    exercício-05 corrigido

---
+++ exercicios/exericio-05-array.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+ <?php
+
+ // Lista de alunos com suas notas
+$alunos = [
+    [
+        'nome' => 'Marcio',
+        'nota1' => 8.0,
+        'nota2' => 7.5,
+        'nota3' => 9.0
+    ],
+    [
+        'nome' => 'Vanessa',
+        'nota1' => 5.0,
+        'nota2' => 6.0,
+        'nota3' => 6.5
+    ],
+    [
+        'nome' => 'Pedro',
+        'nota1' => 9.5,
+        'nota2' => 8.0,
+        'nota3' => 7.5
+    ],
+    [
+        'nome' => 'Ana',
+        'nota1' => 4.5,
+        'nota2' => 5.0,
+        'nota3' => 6.0
+    ],
+    [
+        'nome' => 'Carlos',
+        'nota1' => 7.0,
+        'nota2' => 8.0,
+        'nota3' => 7.5
+    ]
+ ];
+ ?>   
+
+ <?php
+ foreach ($alunos as $aluno){
+    ?>
+  <p>Aluno(a) <?=$aluno["nome"] ?> teve as notas <?=$aluno["nota"]?></p>  
+  <hr>
+</php
+ }
+ ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- exercicios/exericio-05.php
+++ exercicios/exericio-05.php
@@ -1,49 +1,69 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 05</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercício 05 (Funções) - 11/02/2025</title>
+ 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 
+ 
 </head>
-
 <body>
-
-    <h1>Exercício 05</h1>
-<div class="container">
-
-<?php
-$nota1 = 10;
-$nota2 = 7;
-$nota3 = 1;
-  
-$total = $nota1 + $nota2 + $nota3;
-
-// $media = $total /3;
+ 
+  <div class="container">
    
- ?>
-
+<?php
+$nota1 = 9;
+$nota2 = 10;
+$nota3 = 10;
+ 
+$total = $nota1 + $nota2 + $nota3;
+ 
+//$media = $total/3;
+ 
+?>
+ 
+ 
+ 
 <?php
 $media = fn(float $total): float => $total/3;
-
-$resultado = $media($total)
+ 
+$resultado = $media($total);
+ 
 ?>
-
+ 
 <?php
-if($resultado >= 7){?>
- <p class="alert alert-success">A média é <?=$resultado?> o aluno foi aprovado</p>
- <?php  
+ 
+function verificar_situacao($resultado):string {
+  if ($resultado >=7) {
+   return '<span class="badge text-bg-success">Aprovado</span>';
+  } else {
+    return '<span class="badge text-bg-danger">Reprovado</span>';
+  }
+ 
 }
+ 
 ?>
- <p class="alert alert-danger">A média é <?=$resultado?> o aluno foi reprovado</p>
-
-
-
-</div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ 
+<?php
+$mediafinal = number_format($media($total), 2, ".",",");
+?>
+ 
+ 
+ 
+  <p>A média do aluno tem a nota <?=$mediafinal?> o status dele é <?=verificar_situacao($resultado)?></p>
+ 
+ 
+ 
+  </div>
+ 
+ 
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ 
+ 
+ 
 </body>
-
 </html>
+ 
