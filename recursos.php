2435343342bcffc33a4c7eb9d7685c9f3be213cc
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-13 11:19:00 -0300

    adicionando recursos

---
+++ recursos.php
@@ -0,0 +1,14 @@
+<?php
+define("ESCOLA", "Senac");//constante
+$curso = "Téc. Informatica para Internet";//variável
+$tecnologias = ["HTML", "CSS", "JS"];//array
+
+//função
+function verificarIdade(int $valorDaIdade):string {
+    return $valorDaIdade >= 18 ? "maior" : "menor";
+}
+
+
+
+
+?>
