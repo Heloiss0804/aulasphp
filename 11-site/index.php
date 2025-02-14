9bc2d1fb10a566ba622d99f7b64085af93de6034
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-14 08:58:51 -0300

    criando fragmento rodapé

--- 11-site/index.php
+++ 11-site/index.php
@@ -3,17 +3,5 @@
       <p>Esta é a primeira página do nosso site.</p>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, eligendi, quaerat cum quam itaque ea ipsum non quas praesentium autem aliquam eaque nesciunt quidem laudantium adipisci aliquid officiis libero sit?
       </p>
-    </main>
-    <footer>
-      <p>Site criado para o curso Téc. Informática para Internet</p>
-    </footer>
-  </div>
-
-
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    
+      <?php include "rodape.php"?>
