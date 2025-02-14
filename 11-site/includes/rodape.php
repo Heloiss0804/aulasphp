1c9997bf90c0e96c9bdc95d331d3e0a7caa2b82b
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2025-02-14 11:31:18 -0300

    adiciona data por extenso usando itnl

--- 11-site/includes/rodape.php
+++ 11-site/includes/rodape.php
@@ -1,16 +1,25 @@
 </main>
+
+
+<?php
+// Definir o locale como 'pt_BR' (português do Brasil)
+setlocale(LC_TIME, 'pt_BR.UTF-8');
+
+// Criar um objeto DateTime
+$data = new DateTime();
+
+// Usar IntlDateFormatter para formatar a data por extenso
+$fmt = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+$data_extenso = $fmt->format($data);
+
+echo $data_extenso;
+?>
 <footer class="text-center">
   <p>Site criado para o curso Téc. Informática para Internet</p>
-  <?php
 
-  date_default_timezone_set('America/Sao_Paulo');
 
-  echo "Data e hora: " . date('d-m-Y H:i:s');
-  ?>
 </footer>
 </div>
-
-
 
 
 
